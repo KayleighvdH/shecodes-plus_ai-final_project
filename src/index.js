983805f067224259bd4742a86a867609661469db
Kayleigh van der Heijden <146675249+KayleighvdH@users.noreply.github.com>
2025-09-28 23:30:07 +0200

    Improving user experience

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ function generatePoem(event) {
 	event.preventDefault();
 
 	let instructionsInput = document.querySelector("#user-instructions");
-	let prompt = `User instructions are: Generate a romantic poem about ${instructionsInput.value}`;
+	let prompt = `User instructions are: Generate a love poem about ${instructionsInput.value}`;
 	let context =
 		"You are a Poem expert and love to write beautiful and short poems. Your mission is to generate 4 line poems in basic HTML (do not show this) and seperate each line with a <br>, make sure to follow the user instructions. Do not include a title";
 	let apiKey = "007o490fa9c43ta708a74ba7cfaf7bcb";
 	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	let poemElement = document.querySelector("#poem");
+	poemElement.classList.remove("hidden");
+	poemElement.innerHTML = `⏳ Generating a Love Poem about ${instructionsInput.value}`;
 
 	axios.get(apiUrl).then(displayPoem);
 }
